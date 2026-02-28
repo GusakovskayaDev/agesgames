04a0a0b82d833466d8ab390a7abd2676fcfe3969
GusakovskayaDev <gusakovskayadev@gmail.com>
2026-02-28 11:22:14 +0300

    add tech maintenance page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,19 @@
 'use client'
 
+import Image from "next/image";
 import styles from "./page.module.css";
 import { NewsList } from "@/components/NewsList";
 
 export default function Home() {
   return (
     <>
-      <h1>Упс...</h1>
-      <p>Сайт проходит техническое обслуживание</p>
+    <div className={styles.container}>
+      <h1 className={styles.header}>Упс...</h1>
+      <p className={styles.desc}>Сайт проходит техническое обслуживание</p>
+      <Image src={"/tech-maintenance.jpg"} alt={"Техническое обслуживание"} width={600} height={600}/>
       <NewsList />
+    </div>
+      
     </>
   );
 }
